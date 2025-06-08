e7ae5a38d0783d8d4d26219c069a51dd89402989
hemanth18 <hg510443@gmail.com>
2025-06-08 23:02:55 +0530

    Respository folder for all database operations

---
+++ backend/app/repository/user_repository.py
@@ -0,0 +1,38 @@
+"""Repository for user database operations."""
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from app.schemas import CreateStudent
+from app.models import Students, Professors
+from typing import Optional
+from uuid import UUID
+from app.utils import verify_password, hash_password
+
+class UserRepository:
+  
+  def __init__(self, db: Session):
+    self.db = db
+    
+  def createStudent(self, data: CreateStudent) -> Students:
+    student = Students(
+      usn = data.usn,
+      name = data.name,
+      email = data.email,
+      password = hash_password(data.password)
+    )
+    self.db.add(student)
+    self.db.commit()
+    self.db.refresh(student)
+    return student
+    
+  def get_student_by_email(self, email: str) -> Optional[Students]:
+    query = select(Students).where(Students.email == email)
+    return self.db.execute(query).scalar_one_or_none()
+  
+  def get_student_by_id(self, studentid: UUID) -> Optional[Students]:
+    query = select(Students).where(Students.id == studentid)
+    return self.db.execute(query).scalar_one_or_none()
+  
+  def student_exists_by_email(self, email: str) -> bool:
+    """Check if user exists by email"""
+    return self.get_student_by_email(email) is not None
