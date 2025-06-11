95eb8cbe1bfd521eb3a4b45e130b682135c726cb
hemanth18 <hg510443@gmail.com>
2025-06-11 17:07:21 +0530

    Respective Auth & Home pages for the users

---
+++ frontend/app/student/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import { SignupForm } from "@/components/signup-form"
+
+export default function SignupPage() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-[#070707]">
+      <div className="w-full max-w-sm">
+        <SignupForm userType="student"/>
+      </div>
+    </div>
+  )
+}
---
+++ frontend/app/student/home/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to student home page.</h1>
+    </div>
+  )
+}
