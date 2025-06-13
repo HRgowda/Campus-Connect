68bfb5860a0e8c66f075561255678e03aa723af8
hemanth18 <hg510443@gmail.com>
2025-06-14 00:17:04 +0530

    dedicated layout.tsx code for different user management

---
+++ frontend/app/student/home/layout.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { ReactNode } from "react"
+import { Toaster } from "@/components/ui/sonner"
+import AuthProvider from "@/components/AuthProvider"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function StudentLayout({ children }: { children: ReactNode }) {
+  return (
+    <AuthProvider>
+      <SidebarProvider>
+        <AppSidebar userType="student" />
+        <SidebarTrigger />
+        <main>
+          {children}
+        </main>
+        <Toaster />
+      </SidebarProvider>
+    </AuthProvider>
+  )
+}
--- frontend/app/student/home/page.tsx
+++ frontend/app/student/home/page.tsx
@@ -1,7 +1,6 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to student home page.</h1>
+    <div className="text-white">
     </div>
   )
 }
