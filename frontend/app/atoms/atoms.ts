11275103872f49783448a64467d44685f84e698c
hemanth18 <hg510443@gmail.com>
2025-07-27 23:18:52 +0530

    refactor: store professor data in atoms for smoother feedback submission

--- frontend/app/atoms/atoms.ts
+++ frontend/app/atoms/atoms.ts
@@ -17,6 +17,13 @@ type Subject = {
   id: string
 }
 
+export interface Professor {
+  name: string
+  email: string
+  id: string
+}
+
+export const professorAtom = atom<Professor | null>(null)
 export const userAtom = atom<UserAtomType | null>(null)
 export const resourceAtom = atom<ResourceAtomType | null>(null)
 export const selectedSubjectAtom = atom<Subject | null>(null)
