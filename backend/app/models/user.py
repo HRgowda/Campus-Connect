4b9d4e962b98227bf669fc09d7ecd4207defaf30
hemanth18 <hg510443@gmail.com>
2025-08-10 12:26:34 +0530

    add one-to-one relationship between Students and StudentProfile models

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -17,6 +17,8 @@ class Students(Base):
     name: Mapped[str] = mapped_column(String)
 
     ratings = relationship("ProfessorRatings", back_populates="student", cascade="all, delete")
+    
+    profile : Mapped["StudentProfile"] = relationship("StudentProfile", back_populates="student", cascade="all, delete")
 
 class StudentProfile(Base):
     __tablename__ = "student_profiles"
@@ -25,8 +27,12 @@ class StudentProfile(Base):
     bio: Mapped[str] = mapped_column(String)
     location: Mapped[str] = mapped_column(String)
     skills: Mapped[List[str]] = mapped_column(ARRAY(String), default=list)
+    
+    student_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("students.id"), unique=True)
 
     websites: Mapped[List["Website"]] = relationship("Website", back_populates="student_profile", cascade="all, delete")
+    
+    student: Mapped["Students"] = relationship("Students", back_populates="profile")
 
 class Website(Base):
     __tablename__ = "websites"
